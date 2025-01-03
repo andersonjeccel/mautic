02b897629f864a17e052f34c4c6519dc680c997b
Anderson, from Dropsolid <116097999+andersonjeccel@users.noreply.github.com>
2025-01-03 10:46:37 -0300

    moving accent colors to global config

--- app/bundles/CoreBundle/Form/Type/ConfigThemeType.php
+++ app/bundles/CoreBundle/Form/Type/ConfigThemeType.php
@@ -6,6 +6,7 @@ use Mautic\CoreBundle\Form\DataTransformer\ArrayStringTransformer;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 
